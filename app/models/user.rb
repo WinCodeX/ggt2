b6428cd2f9f54fe6ee7d163f1c4af05bd9d6785d
WinCodeX <glenwinterg970@gmail.com>
2025-03-05 17:42:57 +0300

    role based integration for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  enum role: { client: 0, agent: 1, warehouse: 2, rider: 3, admin: 4 }
+      has_secure_password
+      validates :email, presence: true, uniqueness: true
+    
+        
 end
